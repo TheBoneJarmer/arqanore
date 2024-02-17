c16138814b2a99da71ee6d95469cbae0f570b55e
Ruben Labruyere <info@rubenlabruyere.be>
2024-02-17 23:41:04 +0100

    Updated build scripts

--- build-windows.sh
+++ build-windows.sh
@@ -4,10 +4,4 @@ mkdir build 2> /dev/null
 cd build
 
 cmake -DBUILD_SHARED_LIBS=ON -S .. -G "MinGW Makefiles"
-
-if [[ -z $1 ]]
-then
-    cmake --build . -t arqanore
-else
-    cmake --build . -t $1
-fi
+cmake --build .
